f56cadacafd04eeef8b852019090cd1565bf874b
ahmet burak <120136980+AhmetburakDurmus@users.noreply.github.com>
2023-01-17 20:39:28 +0300

    Internet progamciligi vize ödevi, final ödevi ve final database
    Internet progamciligi vize ödevi, final ödevi ve final database göndermek icin yapılan islem

---
+++ İnternet prpgamcılıgı final/WVO web vize ödev/WVOD/EF/YeniDBEntities.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+//SqlConnection baglantı = new SqlConnection("Data Source=localhost\SQLEXPRESS;Initial Catalog=Webvizeödevi;Integrated Security=True");
+namespace WVO.EF
+{
+    public class YeniDBEntities : DbContext
+    {
+        public YeniDBEntities()
+        {
+        }
+        public YeniDBEntities(DbContextOptions<YeniDBEntities> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Webvizeödevi;Integrated Security=True");
+        }
+
+        public DbSet<Models.Ders> Ders { get; set; }
+
+    }
+
+
+
+}
---
+++ İnternet prpgamcılıgı final/WVO web vize ödev/WVOD/Models/OgrenciDers.cs
@@ -0,0 +1,16 @@
+﻿namespace WVO.Models
+{
+    public class OgrenciDers
+    {
+        public int Id { get; set; }
+        public String Ad { get; set; }
+        public String Soyad { get; set; }
+        public String KayitTarih { get; set; }
+        public String ÖgrenciNo { get; set; }
+        public String DTarih { get; set; }
+        public String Bolum { get; set; }
+        public String DersAd { get; set; }
+        public int Kredisi { get; set; }
+        public String OkulYonetimId { get; set; }
+    }
+}
